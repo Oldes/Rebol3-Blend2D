b7499706b90a66c18069e8c0de9130726caa9cd6
Oldes <oldes.huhuman@gmail.com>
2021-12-27 20:35:20 +0100

    FIX: since B2D v0.0.17 there is max value instead of count of modes

--- src/blend2d-command.c
+++ src/blend2d-command.c
@@ -20,7 +20,7 @@ REBOOL fetch_mode(REBSER *cmds, REBCNT index, REBCNT *result, REBCNT start, REBC
 	if (RXT_WORD == type || RXT_LIT_WORD == type) {
 		wrd = RL_FIND_WORD(b2d_arg_words, arg.int32a) - start;
 	} else if (RXT_INTEGER == type) wrd = arg.int64;
-	if (wrd >= 0 && wrd < max) {
+	if (wrd >= 0 && wrd <= max) {
 		result[0] = wrd;
 		return TRUE;
 	}
